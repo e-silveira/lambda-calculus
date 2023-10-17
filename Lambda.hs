26f0591d7c35f1954c06ff4b2a79987fd3443ec2
Eduardo <esilveira@inf.ufsm.br>
2023-10-17 06:05:07 -0300

    Initial commit.

---
+++ Lambda.hs
@@ -0,0 +1,10 @@
+module Lambda where
+
+data Expression = Variable Char
+                | Abstraction Char Expression
+                | Application Expression Expression deriving Show
+
+freeVariables :: Expression -> [Char]
+freeVariables (Variable x) = [x]
+freeVariables (Abstraction x e) = [y | y <- freeVariables e, y /= x]
+freeVariables (Application a b) = freeVariables a ++ freeVariables b
